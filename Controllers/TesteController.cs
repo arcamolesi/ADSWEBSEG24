beefd77f6c47915930d2418f15db16d830e244ac
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-10-18 19:34:31 -0300

    Add project files.

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADSWEBSEG.Controllers
+{
+ 
+    public class TesteController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
