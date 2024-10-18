c84e0826b834215f9925d85c657e868e05ed3a7b
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-10-18 20:48:42 -0300

    Atualização autorização

---
+++ Controllers/FemaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADSWEBSEG.Controllers
+{
+    public class FemaController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public  IActionResult ADS()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Supervisor")]
+        public IActionResult BCC() { 
+            return View();
+        }
+    
+    
+    }
+}
